3934e2c48fe345c3cf75da2fd998fac33f631ebb
Felix Ingrand <felix@laas.fr>
2003-10-21 08:55:14 +0000

    Works on Solaris...

---
+++ config/sparc-solaris.h
@@ -0,0 +1,35 @@
+#include "config.h.in"
+
+#define BIG_ENDIAN
+
+#define DEBUG_FELIX
+#define PRPC_DEBUG
+#define XEDITRESCHECKMESSAGES_AVAILABLE
+#define HAS_SIGACTION
+#define MOTIF_HAS_TEMPLATE_DIALOG
+
+#define USE_MULTI_THREAD
+#define STD_MALLOC
+/* #define PROPICE_MALLOC */
+
+#define INLINE_UNDEFINED
+
+#define HAS_STRERROR
+#define HAS_LRAND48
+#define HAS_ABORT
+#define HAS_FORK
+#define HAS_GETCWD
+#define HAS_UNISTD
+/* #define HAS_SYS_SIGNAL */
+#define HAS_FORK
+#define HAS_TIMER
+#define HAS_SYSTEMINFO
+#define HAS_GETUID
+
+#define HAS_MOTIF_CLASS_INITIALIZE_BUG
+#define HAS_MOTIF_1_2
+#define HAS_UNCOMPRESS "uncompress"
+
+#define CONNECT_TO_IXTET
+
+#define YY_NO_UNPUT
