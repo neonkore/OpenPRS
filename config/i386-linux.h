d60878ea00aaed5cf5f6fa469e893939081a46a4
Felix Ingrand <felix@laas.fr>
2003-12-08 09:14:05 +0000

    New config files

---
+++ config/i386-linux.h
@@ -0,0 +1,24 @@
+#include "config.h.in"
+
+#define LESSTIF
+#define PRPC_DEBUG
+#define XEDITRESCHECKMESSAGES_AVAILABLE
+#define HAS_SIGACTION
+#define MOTIF_HAS_TEMPLATE_DIALOG
+
+#define USE_MULTI_THREAD
+#define MIXED_MALLOC
+
+#define HAS_BUFSIZ
+#define HAS_STRERROR
+#define HAS_LRAND48
+#define HAS_ABORT
+#define HAS_FORK
+#define HAS_GETCWD
+#define HAS_UNISTD
+#define HAS_GETHOSTNAME_DECLARATION
+#define HAS_FORK
+#define HAS_TIMER
+#define HAS_SYSTEMINFO
+#define HAS_GETUID
+
