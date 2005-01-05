fac177ae0b95045f757f206372ceb254997fd0d9
Felix Ingrand <felix@laas.fr>
2005-01-05 13:18:54 +0000

    More and more configure stuff...

--- macro.h
+++ macro.h
@@ -3,7 +3,7 @@
  * 
  * $Id$
  * 
- * Copyright (c) 1991-2003 Francois Felix Ingrand.
+ * Copyright (c) 1991-2004 Francois Felix Ingrand.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -273,12 +273,6 @@ void *realloc1(void *ptr, size_t size);
 
 /* #define GMEXPR_MEXPR(x) ((x)->u.mexpr) */
 /* #define GMEXPR_LMEXPR(x) ((x)->u.lmexpr) */
-
-#ifdef INLINE_UNDEFINED
-#define INLINE
-#else
-#define INLINE inline
-#endif
 
 #define PROTECT_SYSCALL_FROM_EINTR(res,syscall)  while ((res = (syscall)) == -1 &&  (errno == EINTR)) {}
 
