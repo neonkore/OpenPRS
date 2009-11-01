3934112fb0785514b35d92edc16f2999d43ca1ec
Felix Ingrand <felix@laas.fr>
2009-11-01 08:27:42 +0100

    Just to switch branches.

---
+++ gope-main.c
@@ -0,0 +1,31 @@
+#include <gtk/gtk.h>
+ 
+void on_window_destroy (GtkObject *object, gpointer user_data)
+{
+     gtk_main_quit ();
+}
+
+void on_quit_item_activate (GtkObject *object, gpointer user_data)
+{
+     gtk_main_quit ();
+}
+
+int main (int argc, char *argv[])
+{
+     GtkBuilder      *builder; 
+     GtkWidget       *window;
+ 
+     gtk_init (&argc, &argv);
+ 
+     builder = gtk_builder_new ();
+     gtk_builder_add_from_file (builder, "glade/gope.xml", NULL);
+     window = GTK_WIDGET (gtk_builder_get_object (builder, "top_level_window"));
+     gtk_builder_connect_signals (builder, NULL);
+ 
+     g_object_unref (G_OBJECT (builder));
+        
+     gtk_widget_show (window);                
+     gtk_main ();
+ 
+     return 0;
+}
